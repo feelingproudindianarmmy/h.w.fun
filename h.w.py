7dfdd5568045f86e36937c040e4dfa42eb1496a6
feelingproudindianarmy <celestialh4@gmail.com>
2024-12-29 12:43:57 +0545

    calculator

---
+++ h.w.py
@@ -0,0 +1,12 @@
+def add(number):
+    return 2+4
+def subtraction(number):
+    return 6-4
+def multiplication(number):
+    return 7*4
+def divide(number):
+    return 3//7
+print(add(2+4))
+print(subtraction(6-4))
+print(multiplication(7*4))
+print(divide(3//7))
